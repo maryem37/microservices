a8bb0ac9741cff615f6e2bb395b07f8c1390f427
imen kammoun <imen.kammoun@enis.tn>
2026-01-29 22:11:29 +0100

    Mise à jour pom.xml et configuration sécurité

--- conge/src/main/java/tn/enis/conge/configuration/WebSecurityConfiguration.java
+++ conge/src/main/java/tn/enis/conge/configuration/WebSecurityConfiguration.java
@@ -35,6 +35,14 @@ public class WebSecurityConfiguration {
                         .requestMatchers("/api/users/**").permitAll()  // ✅ ADD THIS LINE
                         .requestMatchers("/api/leave/**").permitAll() // TEMPORARY for testing
 
+                        // ✅ Autoriser Swagger
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/index.html"
+                        ).permitAll()
+
                         .requestMatchers("/api/employer/**").hasAuthority(UserRole.Employer.name())
                         .requestMatchers("/api/administration/**").hasAuthority(UserRole.Administration.name())
                         .requestMatchers("/api/teamLeader/**").hasAuthority(UserRole.TeamLeader.name())
